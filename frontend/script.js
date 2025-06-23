48345e8762048df72cc2fd7cfde0cc6d8a5c56ea
Aarthi R <132824439+aarthiramkumar@users.noreply.github.com>
2025-06-23 18:48:28 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       try {
-        const res = await fetch("http://localhost:5000/contact", {  // ✅ Updated here
+        const res = await fetch("https://my-portfolio-backend-4qn2.onrender.com/contact", { // ✅ LIVE URL
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ name, email, message }),
